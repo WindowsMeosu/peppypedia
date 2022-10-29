3bbd0dfb23ce03ed42aba5e3c6853ef52a6cdbd3
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-10-29 23:18:04 +0200

    Update action_page.php

--- docs/action_page.php
+++ docs/action_page.php
@@ -1,4 +1,38 @@
-<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<?php
+// Database connection parameters.
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SampleDB";
+$user=$_POST['uname'];  
+$pass=$_POST['psw']; 
+#echo $user.$pass; 
 
-  $Username = $_POST['Username'];
-  $Password =mysqli_real_escape_string( $_POST['Password']);
+$con=mysql_connect('localhost','root','') or (mysql_error());  
+    mysql_select_db('SampleDB') or die("cannot select DB");
+#echo $con; 
+$query=mysql_query("SELECT * FROM sampletable WHERE username='".$user."' AND password='".$pass."'");  
+    $numrows=mysql_num_rows($query);  
+#echo $numrows;
+if($numrows!=0)  
+    {  
+    while($row=mysql_fetch_assoc($query))  
+    {  
+        $dbusername=$row['username'];  
+        $dbpassword=$row['password']; 
+    }
+    #echo $dbusername.$dbpassword;
+    if($user == $dbusername && $pass == $dbpassword)  
+    {  
+        echo "Valid User .. Successfully Logged In !!";  
+    } else {  
+        echo "Invalid Username or password!";  
+    } 
+    }
+?>
+</body>
+</html>
