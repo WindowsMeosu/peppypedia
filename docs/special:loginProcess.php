3786079058dbe7ae3207e382cc4597b99eda6562
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-10-30 00:12:03 +0200

    Create special:loginProcess.php

---
+++ docs/special:loginProcess.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(isset($_POST['save']))
+{
+    extract($_POST);
+    include 'database.php';
+    $sql=mysqli_query($conn,"SELECT * FROM register where Username='$email' and Password='md5($pass)'");
+    $row  = mysqli_fetch_array($sql);
+    if(is_array($row))
+    {
+        $_SESSION["ID"] = $row['ID'];
+        $_SESSION["Username"]=$row['Username'];
+        header("Location: home.php"); 
+    }
+    else
+    {
+        echo "Request failed... Re-attempting is not recommended.";
+    }
+}
+?>
