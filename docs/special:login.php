81b890c9adab9e99046355a3c09839bdcad210b3
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-10-30 00:08:52 +0200

    Update and rename special:login.html to special:login.php

---
+++ docs/special:login.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+</head>
+<body>
+<div class="signup-form">
+    <form action="special:loginProcess.php" method="post" enctype="multipart/form-data">
+		<h2>Login</h2>
+		<p class="hint-text">Enter Login Details</p>
+        <div class="form-group">
+        	<input type="username" class="form-control" name="Username" placeholder="username" required="required">
+        </div>
+		<div class="form-group">
+            <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
+        </div>
+		<div class="form-group">
+            <button type="submit" name="save" class="btn btn-success btn-lg btn-block">Login</button>
+        </div>
+        <div class="text-center">Don't have an account? <a href="register.php">Register Here</a></div>
+    </form>
+</div>
+</body>
+</html>
