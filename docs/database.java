b4aba235ff337c846bfe78e14a102ffdb26c451b
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-11-04 22:54:33 +0100

    Update database.java

--- docs/database.java
+++ docs/database.java
@@ -29,10 +29,12 @@ $servername = "localhost";
 $username = "root";
 $password = "password";/* Put your password here */
 /* Create connection */
-$conn = new mysqli($servername, $username, $password);
+/* $conn = new mysqli($servername, $username, $password);
 /* Check connection */
-if ($conn->connect_error) {
+/* if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    */
+ 
 /* Create database */
 $sql = "CREATE DATABASE admin";
 if ($conn->query($sql) === TRUE) {
