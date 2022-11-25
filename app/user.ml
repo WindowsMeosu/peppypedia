20688d5622a7ceb9ca570df9ea7b0a2043bfe4fc
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-11-25 01:07:43 +0100

    Update user.ml

--- app/user.ml
+++ app/user.ml
@@ -1,8 +1,9 @@
-fun val user_val = + 1
-fun val page_val = + 1
-fun val docs_val = + 1
-fun val mil_val = + 1
-fun val prof_pic = - 1 = val left + 1 = val right
+val user_val = + 1
+val page_val = + 1
+val docs_val = + 1
+val mil_val = + 1
+val prof_pic = - 1 = val left + 1 = val right
 database
  = square val prof_pic = left - 52
 
+(*values stored in standard ml*)
