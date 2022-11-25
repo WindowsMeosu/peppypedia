772784b6b434259ce4a182357024df828ffb54d9
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-11-25 01:27:49 +0100

    Update userval.ml

--- userval.ml
+++ userval.ml
@@ -1,4 +1,8 @@
-val user_val (x = 1) + x = 1 = x < 0
-fun fac (0 : int) int = 0
- | val user_val < 0 > 0 < 0 > < 0 > < 0 > 0 < 0 > 0 < 0 > 0 < 0 > = 0
-     end
+fun val userval (x = 1) + x = 1 = x < 0
+ | fun fac (0 : int) int = 0
+  | fun val userval < 0 > 0 < 0 > < 0 > < 0 > 0 < 0 > 0 < 0 > 0 < 0 = 0
+    | fun val userval < 0 < 0 < 0 < 0 < 0 < 0 < 0 < 0 < 0 < 0 < 0 < 0 = 0
+      |  if val userval = 1 then < 0
+       | if val userval = 1 then < 0 < 0 < 0 < 0 = x
+        | if val userval = 1 then < 0 + 1 - 1 < 0 + 1 - 1 = z
+           end
