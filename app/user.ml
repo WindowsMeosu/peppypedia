4f6c86e15ee416f2d40faf3ca5f8b69315a44a0f
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2022-11-25 01:06:30 +0100

    Update and rename test.ml to app/user.ml

---
+++ app/user.ml
@@ -0,0 +1,8 @@
+fun val user_val = + 1
+fun val page_val = + 1
+fun val docs_val = + 1
+fun val mil_val = + 1
+fun val prof_pic = - 1 = val left + 1 = val right
+database
+ = square val prof_pic = left - 52
+
