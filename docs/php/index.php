b3ef1f0ce6f4f248f0fa5833ab4f34cc27511bd5
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2023-09-17 01:36:14 +0200

    debug

---
+++ docs/php/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$debugcheck= "check passed";
+$checkif404= "404 check...";
+$if404chpassed= "passed";
+$if404chfailed= "unfortunately failed";
+
+?>
+<b><i><? echo $debugcheck?></b></i>
+<b><i><? echo $if404chpassed $if404chpassed?></b></i>
+<?
+session_destroy();
+<?php ?>
